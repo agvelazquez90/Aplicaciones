c8bce149f521435dd7c17d5c50bf474031b570bc
agvelazquez90 <angel.velazquez@upr.edu>
2017-11-26 12:04:12 -0400

    Update convert.R

--- Coordenadas/convert.R
+++ Coordenadas/convert.R
@@ -1,19 +1,21 @@
 source("x_y.R")
 source("lat_lon.R")
 
-convert <- function(test){
-  if(is.null(test$X)){
-    location = c("Lon", "Lat")
-    d = test[, location]
+convert <- function(df){
+  col_name = toupper(names(df))
+  names(df) <- col_name
+  if(is.null(df$X)){
+    location = c("LON", "LAT")
+    d = df[, location]
     colnames(d) = c("X", "Y")
-    table = processing_lat.lot(d, test)
+    table = processing_lat.lot(d, df)
     return(table)
   }
   else{
     location = c("X", "Y")
-    d = test[, location]
-    colnames(d) = c("Lon", "Lat")
-    table = processing_xy(d, test)
+    d = df[, location]
+    colnames(d) = c("LON", "LAT")
+    table = processing_xy(d, df)
     return(table)
   }
 }
